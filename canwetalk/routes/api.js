13fe45b1944cf358417abea281b3e220a9482393
Amy Hong <amyhong0502@gmail.com>
2018-09-16 11:01:41 -0700

    feat: set routes for api and webpage

--- canwetalk/routes/api.js
+++ canwetalk/routes/api.js
@@ -5,12 +5,31 @@ let userController = require('../controllers/userController');
 let sleepController = require('../controllers/sleepController');
 let exerciseController = require('../controllers/exerciseController');
 
+// User
 router.post('/user/create', (req, res) => {
   userController.create_user(req, res);
 });
 
 router.post('/user/findUserByUserId', (req, res) => {
   userController.find_user_by_userId(req, res);
+});
+
+// Sleep
+router.post('/sleep/create', (req, res) => {
+  sleepController.create_sleep(req, res);
+});
+
+router.get('/sleep/read', (req, res) => {
+  sleepController.list_all(req, res);
+});
+
+// Exercise
+router.post('/exercise/create', (req, res) => {
+  exerciseController.create_exercise(req, res);
+});
+
+router.get('/exercise/read', (req, res) => {
+  exerciseController.list_all(req, res);
 });
 
 module.exports = router;
--- canwetalk/routes/index.js
+++ canwetalk/routes/index.js
@@ -1,9 +1,19 @@
 let express = require('express');
 let router = express.Router();
 
+let userController = require('../controllers/userController');
+let sleepController = require('../controllers/sleepController');
+let exerciseController = require('../controllers/exerciseController');
+
+let dashboardController = require('../controllers/dashboardController');
+
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/dashboard', (req, res) => {
+  dashboardController.list_all(req, res);
 });
 
 module.exports = router;
